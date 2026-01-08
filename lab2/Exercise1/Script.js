c9a2685e253b5e3e1cbaa8570fe1ba770d7d163a
vedesh-18 <vedeshmaddali@gmail.com>
2026-01-08 22:58:27 +0530

    lab 1 commit

--- lab2/Exercise1/Script.js
+++ lab2/Exercise1/Script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -24,3 +25,31 @@ ctx.font = 'bold 28px sans-serif';
 ctx.fillStyle = '#000';
 ctx.textAlign = 'center';
 ctx.fillText('HTML5 Canvas', 250, 260);
+=======
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+
+ctx.fillStyle = 'green';
+ctx.fillRect(50, 40, 120, 80);
+
+
+ctx.beginPath();
+ctx.arc(400, 100, 50, 0, Math.PI * 2);
+ctx.fillStyle = 'blue';
+ctx.fill();
+
+
+ctx.beginPath();
+ctx.moveTo(50, 200);
+ctx.lineTo(450, 200);
+ctx.strokeStyle = '#000';
+ctx.lineWidth = 2;
+ctx.stroke();
+
+
+ctx.font = 'bold 28px sans-serif';
+ctx.fillStyle = '#000';
+ctx.textAlign = 'center';
+ctx.fillText('HTML5 Canvas', 250, 260);
+>>>>>>> aa10dc0e3687c52e445df8d251edd0c7082bdd1a
